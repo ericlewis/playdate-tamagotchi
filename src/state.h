2865e5202fc49cf5f1146a56bad7bf607f0796df
Eric Lewis <ericlewis777@gmail.com>
2022-07-04 12:19:59 -0400

    initial commit

---
+++ src/state.h
@@ -0,0 +1,26 @@
+/*
+ * Playdate-Tamagotchi - A Tamagotchi P1 emulator for Playdate
+ *
+ * Copyright (C) 2022 Eric Lewis <ericlewis777@gmail.com>
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+ */
+#ifndef _STATE_H_
+#define _STATE_H_
+
+void state_save();
+void state_load();
+
+#endif /* _STATE_H_ */
